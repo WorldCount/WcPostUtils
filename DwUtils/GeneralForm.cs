74d038da2d0b8951986b456e9834fe8addd13f54
WorldCount <World.Count@yandex.ru>
2021-03-27 22:28:57 +0300

    Начальная настройка главного окна

--- DwUtils/GeneralForm.cs
+++ DwUtils/GeneralForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace DwUtils
 {
-    public partial class Form1 : Form
+    public partial class GeneralForm : Form
     {
-        public Form1()
+        public GeneralForm()
         {
             InitializeComponent();
         }
