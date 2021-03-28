d9611c58f1b1ed94acd7770cf4798a5a5b299945
WorldCount <World.Count@yandex.ru>
2021-03-28 13:05:11 +0300

    Добавлен виджет текстбокса с рамкой

---
+++ Wc32Api/Widgets/TextBoxs/BorderTextBox.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Wc32Api.Widgets.TextBoxs
+{
+    public class BorderTextBox : TextBox
+    {
+
+        public int BorderHeight
+        {
+            get => Controls[0].Height;
+            set => Controls[0].Height = value;
+        }
+
+        public Color EnterColor { get; set; } = Color.Firebrick;
+        public Color LeaveColor { get; set; } = Color.SteelBlue;
+
+        public BorderTextBox()
+        {
+            Win32.DrawingControl.SetDoubleBuffered(this);
+
+            BorderStyle = BorderStyle.None;
+            Controls.Add(new Label { Dock = DockStyle.Bottom });
+
+            Controls[0].BackColor = LeaveColor;
+            BorderHeight = 2;
+        }
+
+        protected override void OnLeave(EventArgs e)
+        {
+            base.OnLeave(e);
+            Controls[0].BackColor = LeaveColor;
+        }
+
+        protected override void OnEnter(EventArgs e)
+        {
+            base.OnEnter(e);
+            Controls[0].BackColor = EnterColor;
+        }
+    }
+}
