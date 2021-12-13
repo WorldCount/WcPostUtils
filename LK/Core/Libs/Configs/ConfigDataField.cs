0b66dd96b0487b8b37805b27cc1798bf15091d8d
WorldCount <World.Count@yandex.ru>
2021-12-13 10:17:44 +0300

    Настройки с данными для парсинга строк

---
+++ LK/Core/Libs/Configs/ConfigDataField.cs
@@ -0,0 +1,10 @@
+﻿
+namespace LK.Core.Libs.Configs
+{
+    public class ConfigDataField
+    {
+        public ConfigDataFieldType Type { get; set; }
+        public string Desc { get; set; }
+        public int NumColumn { get; set; }
+    }
+}
---
+++ LK/Core/Libs/Configs/ConfigDataFieldType.cs
@@ -0,0 +1,29 @@
+﻿
+namespace LK.Core.Libs.Configs
+{
+    public enum ConfigDataFieldType
+    {
+        ListDate,
+        ListNum,
+        TransType,
+        FirmName,
+        Inn,
+        Kpp,
+        NumDog,
+        Barcode,
+        Type,
+        Category,
+        MassRate,
+        NoticeRate,
+        AviaRate,
+        Value,
+        ValueRate,
+        Status,
+        StatusMessage,
+        ReceptDate,
+        Operator,
+        Ops,
+        Index,
+        Address
+    }
+}
