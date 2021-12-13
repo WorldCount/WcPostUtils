6c2927e7e22565b13b0bac6fafef8a40db51442c
WorldCount <World.Count@yandex.ru>
2021-12-13 11:37:40 +0300

    Исправление парсинга даты при повторной загрузке данных из файла

--- LK/Core/Models/Raw/RawData.cs
+++ LK/Core/Models/Raw/RawData.cs
@@ -2,7 +2,6 @@
 using System.Text.RegularExpressions;
 using LK.Core.Models.DB;
 using LK.Core.Models.Types;
-using LK.Core.Store.Manager;
 using LK.Core.Store.Manager.FileManager;
 using NPOI.SS.UserModel;
 
@@ -66,49 +65,113 @@ namespace LK.Core.Models.Raw
 
         private void ParseFirm()
         {
-            Inn = _row.GetCell(_cm.Inn.NumColumn, MissingCellPolicy.RETURN_BLANK_AS_NULL).ToString().Trim();
-            Kpp = _row.GetCell(_cm.Kpp.NumColumn, MissingCellPolicy.RETURN_BLANK_AS_NULL).ToString().Trim();
-            FirmName = _row.GetCell(_cm.FirmName.NumColumn, MissingCellPolicy.RETURN_BLANK_AS_NULL).ToString().Trim();
-            FirmName = Utils.TrimName(FirmName);
-            Contract = _row.GetCell(_cm.Contract.NumColumn, MissingCellPolicy.RETURN_BLANK_AS_NULL).ToString().Trim();
+            try
+            {
+                Inn = _row.GetCell(_cm.Inn.NumColumn, MissingCellPolicy.RETURN_BLANK_AS_NULL).ToString().Trim();
+                Kpp = _row.GetCell(_cm.Kpp.NumColumn, MissingCellPolicy.RETURN_BLANK_AS_NULL).ToString().Trim();
+                FirmName = _row.GetCell(_cm.FirmName.NumColumn, MissingCellPolicy.RETURN_BLANK_AS_NULL).ToString().Trim();
+                FirmName = Utils.TrimName(FirmName);
+                Contract = _row.GetCell(_cm.Contract.NumColumn, MissingCellPolicy.RETURN_BLANK_AS_NULL).ToString().Trim();
+            }
+            catch (Exception e)
+            {
+                Exception = e;
+                throw;
+            }
         }
 
         private void ParseDate()
         {
-            Date = _row.GetCell(_cm.ListDate.NumColumn, MissingCellPolicy.RETURN_BLANK_AS_NULL).DateCellValue;
-            ReceptDate = _row.GetCell(_cm.ReceptDate.NumColumn, MissingCellPolicy.RETURN_BLANK_AS_NULL).DateCellValue;
+            try
+            {
+                DateTime? rawDate = GetCellDateValue(_row.GetCell(_cm.ListDate.NumColumn, MissingCellPolicy.RETURN_BLANK_AS_NULL));
+                DateTime? rawReceptDate = GetCellDateValue(_row.GetCell(_cm.ReceptDate.NumColumn, MissingCellPolicy.RETURN_BLANK_AS_NULL));
+
+                if (rawDate == null || rawReceptDate == null)
+                {
+                    throw new NullReferenceException("Ошибка парсинга даты");
+                }
+
+                Date = (DateTime) rawDate;
+                ReceptDate = (DateTime) rawReceptDate;
+            }
+            catch (Exception e)
+            {
+                Exception = e;
+                throw;
+            }
         }
 
         private void ParseList()
         {
-            ListNum = (int)_row.GetCell(_cm.ListNum.NumColumn, MissingCellPolicy.RETURN_BLANK_AS_NULL).NumericCellValue;
-            Type = _row.GetCell(_cm.Type.NumColumn, MissingCellPolicy.RETURN_BLANK_AS_NULL).ToString().Trim();
-            Category = _row.GetCell(_cm.Category.NumColumn, MissingCellPolicy.RETURN_BLANK_AS_NULL).ToString().Trim();
-            TransType = _row.GetCell(_cm.TransType.NumColumn, MissingCellPolicy.RETURN_BLANK_AS_NULL).ToString().Trim();
+            try
+            {
+                ListNum = (int)_row.GetCell(_cm.ListNum.NumColumn, MissingCellPolicy.RETURN_BLANK_AS_NULL).NumericCellValue;
+                Type = _row.GetCell(_cm.Type.NumColumn, MissingCellPolicy.RETURN_BLANK_AS_NULL).ToString().Trim();
+                Category = _row.GetCell(_cm.Category.NumColumn, MissingCellPolicy.RETURN_BLANK_AS_NULL).ToString().Trim();
+                TransType = _row.GetCell(_cm.TransType.NumColumn, MissingCellPolicy.RETURN_BLANK_AS_NULL).ToString().Trim();
+            }
+            catch (Exception e)
+            {
+                Exception = e;
+                throw;
+            }
         }
 
         private void ParseRpo()
         {
-            Barcode = _row.GetCell(_cm.Barcode.NumColumn, MissingCellPolicy.RETURN_BLANK_AS_NULL).ToString().Trim();
-            MassRate = _row.GetCell(_cm.MassRate.NumColumn, MissingCellPolicy.RETURN_BLANK_AS_NULL).NumericCellValue / 100;
-            ServiceRate = _row.GetCell(_cm.NoticeRate.NumColumn, MissingCellPolicy.RETURN_BLANK_AS_NULL).NumericCellValue / 100;
-            AviaRate = _row.GetCell(_cm.AviaRate.NumColumn, MissingCellPolicy.RETURN_BLANK_AS_NULL).NumericCellValue / 100;
-            Value = _row.GetCell(_cm.Value.NumColumn, MissingCellPolicy.RETURN_BLANK_AS_NULL).NumericCellValue / 100;
-            ValueRate = _row.GetCell(_cm.ValueRate.NumColumn, MissingCellPolicy.RETURN_BLANK_AS_NULL).NumericCellValue / 100;
-            Value = _row.GetCell(_cm.Value.NumColumn, MissingCellPolicy.RETURN_BLANK_AS_NULL).NumericCellValue / 100;
+            try
+            {
+                Barcode = _row.GetCell(_cm.Barcode.NumColumn, MissingCellPolicy.RETURN_BLANK_AS_NULL).ToString().Trim();
+                MassRate = _row.GetCell(_cm.MassRate.NumColumn, MissingCellPolicy.RETURN_BLANK_AS_NULL).NumericCellValue / 100;
+                ServiceRate = _row.GetCell(_cm.NoticeRate.NumColumn, MissingCellPolicy.RETURN_BLANK_AS_NULL).NumericCellValue / 100;
+                AviaRate = _row.GetCell(_cm.AviaRate.NumColumn, MissingCellPolicy.RETURN_BLANK_AS_NULL).NumericCellValue / 100;
+                Value = _row.GetCell(_cm.Value.NumColumn, MissingCellPolicy.RETURN_BLANK_AS_NULL).NumericCellValue / 100;
+                ValueRate = _row.GetCell(_cm.ValueRate.NumColumn, MissingCellPolicy.RETURN_BLANK_AS_NULL).NumericCellValue / 100;
+                Value = _row.GetCell(_cm.Value.NumColumn, MissingCellPolicy.RETURN_BLANK_AS_NULL).NumericCellValue / 100;
 
-            OpsIndex = _row.GetCell(_cm.Ops.NumColumn, MissingCellPolicy.RETURN_BLANK_AS_NULL).ToString().Trim();
-            IndexTo = _row.GetCell(_cm.Index.NumColumn, MissingCellPolicy.RETURN_BLANK_AS_NULL).ToString().Trim();
-            Address = _row.GetCell(_cm.Address.NumColumn, MissingCellPolicy.RETURN_BLANK_AS_NULL).ToString().Trim();
+                OpsIndex = _row.GetCell(_cm.Ops.NumColumn, MissingCellPolicy.RETURN_BLANK_AS_NULL).ToString().Trim();
+                IndexTo = _row.GetCell(_cm.Index.NumColumn, MissingCellPolicy.RETURN_BLANK_AS_NULL).ToString().Trim();
+                Address = _row.GetCell(_cm.Address.NumColumn, MissingCellPolicy.RETURN_BLANK_AS_NULL).ToString().Trim();
 
-            Status = _row.GetCell(_cm.Status.NumColumn, MissingCellPolicy.RETURN_BLANK_AS_NULL).ToString().Trim();
-            Reason = _row.GetCell(_cm.StatusMessage.NumColumn, MissingCellPolicy.RETURN_BLANK_AS_NULL).ToString().Trim();
+                Status = _row.GetCell(_cm.Status.NumColumn, MissingCellPolicy.RETURN_BLANK_AS_NULL).ToString().Trim();
+                Reason = _row.GetCell(_cm.StatusMessage.NumColumn, MissingCellPolicy.RETURN_BLANK_AS_NULL).ToString().Trim();
+            }
+            catch (Exception e)
+            {
+                Exception = e;
+                throw;
+            }
         }
 
         private void ParseOperator()
         {
-            string o = _row.GetCell(_cm.Oper.NumColumn, MissingCellPolicy.RETURN_BLANK_AS_NULL).ToString().Trim();
-            Operator = o.Contains("  ") ? Regex.Replace(o, "[ ]+", " ") : o;
+            try
+            {
+                string o = _row.GetCell(_cm.Oper.NumColumn, MissingCellPolicy.RETURN_BLANK_AS_NULL).ToString().Trim();
+                Operator = o.Contains("  ") ? Regex.Replace(o, "[ ]+", " ") : o;
+            }
+            catch (Exception e)
+            {
+                Exception = e;
+                throw;
+            }
+        }
+
+        private DateTime? GetCellDateValue(ICell cellValue)
+        {
+            if (cellValue == null)
+            {
+                // Cell doesn't have any value
+                return null;
+            }
+
+            if (cellValue.CellType == CellType.Numeric)
+            {
+                return DateTime.FromOADate(cellValue.NumericCellValue);
+            }
+
+            return cellValue.DateCellValue;
         }
 
         #endregion
@@ -128,7 +191,7 @@ namespace LK.Core.Models.Raw
             catch (Exception e)
             {
                 Exception = e;
-                return false;
+                throw;
             }
 
             return true;
